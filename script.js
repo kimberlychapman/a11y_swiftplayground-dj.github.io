a88a5c62b167d0c04d7078d171566606e58594df
kimberlychapman <63875440+kimberlychapman@users.noreply.github.com>
2020-12-19 19:30:07 -0600

    added dropboxes sound hover effects and set up track loop counter

--- script.js
+++ script.js
@@ -41,27 +41,29 @@ function countLoops(){
     document.getElementById('test').style.borderBottom="solid white 2px";
     document.getElementById('test').style.borderTop="solid white 2px";
     document.getElementById('test').style.fontWeight="bold";
-    document.getElementById('test').style.backgroundColor="black";
+    // document.getElementById('test').style.backgroundColor="black";
 }
+
 function totalLoops(){
-    document.getElementById('test').innerHTML="Command";
-    document.getElementById('test').style.backgroundColor="#333";
-    document.getElementById('test').style.color="#eee";
+    document.getElementById('test').innerHTML="Queue";
+    // document.getElementById('test').style.backgroundColor="#333";
+    document.getElementById('test').style.color="eee";
     document.getElementById('test').style.borderBottom="solid #222 1px";
     document.getElementById('test').style.fontWeight="normal";
     document.getElementById('test').style.borderTop="transparent";
 }
+
 function runTest(){
     document.getElementById('test1').innerHTML="if &nbsp !minors {";
     document.getElementById('test1').style.color="aqua";
     document.getElementById('test1').style.borderBottom="solid aqua 2px";
     document.getElementById('test1').style.borderTop="solid aqua 2px";
     document.getElementById('test1').style.fontWeight="bold";
-    document.getElementById('test1').style.backgroundColor="black";
+    // document.getElementById('test1').style.backgroundColor="black";
 }
 function endTest(){
-    document.getElementById('test1').innerHTML="Command";
-    document.getElementById('test1').style.backgroundColor="#333";
+    document.getElementById('test1').innerHTML="Queue";
+    // document.getElementById('test1').style.backgroundColor="#333";
     document.getElementById('test1').style.color="#eee";
     document.getElementById('test1').style.borderBottom="solid #222 1px";
     document.getElementById('test1').style.fontWeight="normal";
@@ -74,12 +76,12 @@ function getTrack(){
     document.getElementById('test0').style.borderBottom="solid lime 2px";
     document.getElementById('test0').style.borderTop="solid lime 2px";
     document.getElementById('test0').style.fontWeight="bold";
-    document.getElementById('test0').style.backgroundColor="black";   
+    // document.getElementById('test0').style.backgroundColor="black";   
 }
 
 function stopTrack(){
-    document.getElementById('test0').innerHTML="Command";
-    document.getElementById('test0').style.backgroundColor="#333";
+    document.getElementById('test0').innerHTML="Queue";
+    // document.getElementById('test0').style.backgroundColor="#333";
     document.getElementById('test0').style.color="#eee";
     document.getElementById('test0').style.borderBottom="solid #222 1px";
     document.getElementById('test0').style.fontWeight="normal";
@@ -92,12 +94,12 @@ function playExplicit(){
     document.getElementById('test2').style.borderBottom="solid red 2px";
     document.getElementById('test2').style.borderTop="solid red 2px";
     document.getElementById('test2').style.fontWeight="bold";
-    document.getElementById('test2').style.backgroundColor="black";   
+    // document.getElementById('test2').style.backgroundColor="black";   
 }
 
 function stopExplicit(){
-    document.getElementById('test2').innerHTML="Command";
-    document.getElementById('test2').style.backgroundColor="#333";
+    document.getElementById('test2').innerHTML="Queue";
+    // document.getElementById('test2').style.backgroundColor="#333";
     document.getElementById('test2').style.color="#eee";
     document.getElementById('test2').style.borderBottom="solid #222 1px";
     document.getElementById('test2').style.fontWeight="normal";
@@ -110,12 +112,12 @@ function elseTrack(){
     document.getElementById('test3').style.borderBottom="solid yellow 2px";
     document.getElementById('test3').style.borderTop="solid yellow 2px";
     document.getElementById('test3').style.fontWeight="bold";
-    document.getElementById('test3').style.backgroundColor="black";   
+    // document.getElementById('test3').style.backgroundColor="black";   
 }
 
 function elsePlay(){
-    document.getElementById('test3').innerHTML="Command";
-    document.getElementById('test3').style.backgroundColor="#333";
+    document.getElementById('test3').innerHTML="Queue";
+    // document.getElementById('test3').style.backgroundColor="#333";
     document.getElementById('test3').style.color="#eee";
     document.getElementById('test3').style.borderBottom="solid #222 1px";
     document.getElementById('test3').style.fontWeight="normal";
@@ -128,12 +130,12 @@ function playClean(){
     document.getElementById('test4').style.borderBottom="solid magenta 2px";
     document.getElementById('test4').style.borderTop="solid magenta 2px";
     document.getElementById('test4').style.fontWeight="bold";
-    document.getElementById('test4').style.backgroundColor="black";   
+    // document.getElementById('test4').style.backgroundColor="black";   
 }
 
 function stopClean(){
-    document.getElementById('test4').innerHTML="Command";
-    document.getElementById('test4').style.backgroundColor="#333";
+    document.getElementById('test4').innerHTML="Queue";
+    // document.getElementById('test4').style.backgroundColor="#333";
     document.getElementById('test4').style.color="#eee";
     document.getElementById('test4').style.borderBottom="solid #222 1px";
     document.getElementById('test4').style.fontWeight="normal";
@@ -169,4 +171,47 @@ function showCode5(){
     document.getElementById('thing10').style.border="1px solid magenta";
 }
 
+function playSound1(){
+    var firstsound=document.getElementById('minors');
+    firstsound.play();
+}
+
+function stopSound1(){
+    var firstsound=document.getElementById('minors');
+    firstsound.pause();
+    firstsound.currentTime = 0;
+}
+
+function playSound2(){
+    var nextsound=document.getElementById('college');
+    nextsound.play();
+}
+
+function stopSound2(){
+    var nextsound=document.getElementById('college');
+    nextsound.pause();
+   nextsound.currentTime = 0;
+}
+
+function playSound3(){
+    var thensound=document.getElementById('college');
+    thensound.play();
+}
+
+function stopSound3(){
+    var thensound=document.getElementById('college');
+    thensound.pause();
+   thensound.currentTime = 0;
+}
+
+function playSound4(){
+    var finallysound=document.getElementById('college');
+    finallysound.play();
+}
+
+function stopSound4(){
+    var finallysound=document.getElementById('college');
+    finallysound.pause();
+   finallysound.currentTime = 0;
+}
 // 
